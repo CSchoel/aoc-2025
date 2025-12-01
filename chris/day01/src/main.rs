4dc043e2757459d4b4abb712e98a2d831fede71b
Christopher Schölzel <christopher.schoelzel@languagetool.org>
2025-12-01 16:45:39 +0100

    reads and splits input strings

--- chris/day01/src/main.rs
+++ chris/day01/src/main.rs
@@ -1,11 +1,21 @@
 use std::env;
+use std::fmt::Error;
 use std::fs;
 use std::path::Path;
+use std::string;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_path: &Path = Path::new("input.txt");
     println!("{}", input_path.display());
     let contents = fs::read_to_string(input_path).expect("Test");
-    println!("{contents}");
+    for l in contents.lines() {
+        let mut chars = l.trim().chars();
+        let direction = match chars.next() {
+            Some(c) => c,
+            None => continue, // ignore empyt lines
+        };
+        let number: String = chars.collect();
+        println!("{:?} {:?}", direction, number);
+    }
 }
