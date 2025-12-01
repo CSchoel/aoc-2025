6ae6d9497dfe17ad6a76ba9aeaaddc6c1b2e036c
Stefan Viol <stefan@stevio.de>
2025-12-01 21:51:10 +0100

    Stefan (day01): first star

--- stefan/day01/src/main.cpp
+++ stefan/day01/src/main.cpp
@@ -1,20 +1,40 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main() {
   ifstream file("day01/rotations.txt");
   string line;
-  // int dialStart = 50;
-
+  int dialPosition = 50;
+  int nullCounter = 0;
   if (file.is_open()) {
     while (getline(file, line)) {
-      cout << line << endl;
+      if (line.empty()) {
+        continue;
+      }
+      auto direction = line.substr(0, 1);
+      auto steps = line.substr(1, line.length() - 1);
+      auto stepCount = stoi(steps);
+      if (direction == "L") {
+        dialPosition -= stepCount;
+      } else if (direction == "R") {
+        dialPosition += stepCount;
+      }
+      if (dialPosition < 0) {
+        dialPosition = dialPosition % -100;
+      } else if (dialPosition > 99) {
+        dialPosition = dialPosition % 100;
+      }
+      if (dialPosition == 0) {
+        nullCounter++;
+      }
     }
     file.close();
   } else {
     cerr << "Unable to open file" << endl;
   }
+  cout << "The password is: " << nullCounter << endl;
   return 0;
 }
