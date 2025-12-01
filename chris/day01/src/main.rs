5fe54d8e81447f0f0905286aaf62144378c9610e
Christopher Schölzel <christopher.schoelzel@languagetool.org>
2025-12-01 20:13:38 +0100

    adds implementation for part 2

--- chris/day01/src/main.rs
+++ chris/day01/src/main.rs
@@ -18,6 +18,31 @@ fn parse_input(input: String) -> Vec<(char, u16)> {
     vector
 }
 
+fn count_zero_crossings(instructions: Vec<(char, u16)>) -> i16 {
+    let mut dial: i16 = 50;
+    let mut zero_crossings = 0;
+    for (d, x) in instructions {
+        let delta = match d {
+            'R' => x as i16,
+            'L' => -(x as i16),
+            _ => panic!("Incorrect direction character: {d}"),
+        };
+        // case 1: crossings due to a full turn
+        zero_crossings += delta.abs() / 100;
+        // case 2: crossings due to a partial turn
+        // Partial turns starting at 0 can never lead to a crossing
+        // Otherwise, we can detect a crossing if they end up exactly
+        // at zero or outside the [0, 99] range
+        let remaining = delta % 100; // remaining partial turn
+        if dial != 0 && (dial + remaining <= 0 || dial + remaining >= 100) {
+            zero_crossings += 1;
+        }
+        dial = (dial + remaining).rem_euclid(100);
+        println!("Dial: {dial}, Zero crossings: {zero_crossings}");
+    }
+    zero_crossings
+}
+
 fn count_zero_rests(instructions: Vec<(char, u16)>) -> i16 {
     let mut dial: i16 = 50;
     let mut zeros = 0;
@@ -40,6 +65,6 @@ fn main() {
     let input_path: &Path = Path::new("input.txt");
     let contents = fs::read_to_string(input_path).expect("Test");
     let data = parse_input(contents);
-    let zeros = count_zero_rests(data);
+    let zeros = count_zero_crossings(data);
     println!("{zeros} zero crossings found!")
 }
