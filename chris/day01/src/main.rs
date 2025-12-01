dcbb114ddd3b3cd40ffe164074c9d11a72ff4296
Christopher Schölzel <christopher.schoelzel@languagetool.org>
2025-12-01 15:22:59 +0100

    adds code to load sample input

--- chris/day01/src/main.rs
+++ chris/day01/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_path: &Path = Path::new("input.txt");
+    println!("{}", input_path.display());
+    let contents = fs::read_to_string(input_path).expect("Test");
+    println!("{contents}");
 }
