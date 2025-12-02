d9c33824bf83e0b4053661fcc19506e5bb1d275a
Christopher Schölzel <christopher.schoelzel@languagetool.org>
2025-12-02 10:24:20 +0100

    implements invalid ID sum

--- chris/day02/src/main.rs
+++ chris/day02/src/main.rs
@@ -1,3 +1,58 @@
+//! Solves day 2 of Advent of Code 2025
+
+use std::{
+    collections::btree_map::Range, fs, num::Saturating, ops::Div, path::Path, process::exit,
+};
+
+/// Parses puzzle input for day 2
+fn parse_input(text: &str) -> Result<Vec<(u32, u32)>, &str> {
+    let mut result: Vec<(u32, u32)> = Vec::new();
+    for range in text.split(',') {
+        let mut split = range.split("-");
+        let start = match split.next().and_then(|s| s.parse::<u32>().ok()) {
+            Some(x) => x,
+            None => return Err("Malformed line"),
+        };
+        println!("{:?}", start);
+        let end = match split.next().and_then(|s| s.parse::<u32>().ok()) {
+            Some(x) => x,
+            None => return Err("Malformed line"),
+        };
+        result.push((start, end));
+        println!("{:?}", end);
+    }
+    Ok(result)
+}
+
+/// Find the sum of all invalid IDs
+fn sum_invalid_ids(data: Vec<(u32, u32)>) -> u32 {
+    let mut invalid: Saturating<u32> = Saturating(0);
+    for (start, end) in data {
+        for i in start..end + 1 {
+            let str = i.to_string();
+            if str.len() % 2 != 0 {
+                continue;
+            }
+            let (left, right) = str.split_at(str.len().div(2));
+            if left.eq(right) {
+                invalid += i;
+            }
+        }
+    }
+    invalid.0
+}
+
+/// Loads the file `input.txt` and prints the puzzle solution.
 fn main() {
-    println!("Hello, world!");
+    let input_path: &Path = Path::new("input.txt");
+    let contents: String = match fs::read_to_string(input_path) {
+        Ok(str) => str,
+        Err(err) => panic!("Malformed input {err:?}"),
+    };
+    let data = match parse_input(&contents) {
+        Ok(x) => x,
+        Err(msg) => panic!("Wrong input!"),
+    };
+    let result = sum_invalid_ids(data);
+    println!("Sum: {:?}", result);
 }
