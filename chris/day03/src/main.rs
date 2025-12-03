b81baffe1c21f091550f6350ed52be9a172c6e4b
Christopher Schölzel <christopher.schoelzel@languagetool.org>
2025-12-03 23:32:50 +0100

    bugfix: needed to reverse direction for finding max

--- chris/day03/src/main.rs
+++ chris/day03/src/main.rs
@@ -31,13 +31,20 @@ fn parse_input(text: &str) -> Vec<Vec<u8>> {
 /// assert_eq!(max_joltage(&[8, 2, 3, 9]), 89)
 /// ```
 fn max_joltage(bank: &[u8]) -> u8 {
+    debug!("Calculating max joltage for {bank:?}");
     // find first digit
     let first_range = bank.get(0..bank.len().saturating_sub(1));
-    let first = first_range.and_then(|rng| rng.iter().enumerate().max_by_key(|&(_, digit)| *digit));
+    let first = first_range.and_then(|rng| {
+        rng.iter()
+            .enumerate()
+            .rev()
+            .max_by_key(|&(_, digit)| *digit)
+    });
     let (first_idx, first_val) = match first {
         Some((idx, val)) => (idx, val),
         None => return 0,
     };
+    debug!("First index: {first_idx}, First value: {first_val}");
     let second_range = bank.get(first_idx.saturating_add(1)..bank.len());
     let second = second_range.and_then(|rng| rng.iter().max());
     let second_val = match second {
@@ -65,6 +72,13 @@ fn main() {
     };
     let input = parse_input(&contents);
     info!("Parsed input: {input:?}");
+    // let test = max_joltage(&[
+    //     1, 1, 2, 2, 3, 2, 8, 3, 7, 7, 2, 2, 7, 5, 6, 5, 2, 9, 4, 5, 7, 3, 5, 2, 5, 6, 6, 8, 2, 4,
+    //     6, 2, 5, 2, 9, 2, 5, 1, 6, 4, 2, 4, 6, 2, 8, 7, 9, 8, 7, 7, 8, 4, 1, 2, 2, 6, 9, 3, 9, 8,
+    //     3, 8, 6, 9, 9, 2, 2, 2, 2, 2, 5, 7, 6, 4, 7, 3, 2, 3, 8, 2, 2, 2, 3, 6, 7, 7, 2, 5, 2, 2,
+    //     9, 2, 9, 2, 4, 7, 6, 7, 7, 6,
+    // ]);
+    // info!("Test result: {test}");
     let result = sum_max_joltages(&input);
     info!("Result: {result}");
 }
