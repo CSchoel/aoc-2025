fd14950d49f4b8e9d27b32b5417519169be1ef58
Christopher Schölzel <christopher.schoelzel@languagetool.org>
2025-12-07 15:07:21 +0100

    adds first solution attempt for day 4

--- chris/day04/src/main.rs
+++ chris/day04/src/main.rs
@@ -1,5 +1,11 @@
 //! Solve day 4 of Advent of Code 2025
-use std::{fs, num::Saturating, path::Path, process::exit};
+use std::{
+    fs,
+    num::Saturating,
+    ops::{Div, Rem},
+    path::Path,
+    process::exit,
+};
 
 use log::{debug, error, info};
 
@@ -25,6 +31,14 @@ impl CharMatrix {
         self.matrix.get(idx.0).copied()
     }
 
+    /// Get element at position or None, allow negative inputs
+    fn at_signed(&self, row: isize, col: isize) -> Option<char> {
+        if row < 0 || col < 0 {
+            return None;
+        }
+        self.at(row as usize, col as usize)
+    }
+
     /// Get number of rows
     const fn rows(&self) -> usize {
         self.matrix.len().saturating_div(self.columns)
@@ -41,6 +55,36 @@ fn parse_input(text: &str) -> Option<CharMatrix> {
     Some(CharMatrix { columns, matrix })
 }
 
+/// Solves part 1 of the puzzle
+fn count_movable(mat: &CharMatrix) -> usize {
+    (1..mat.matrix.len())
+        .map(|idx| (idx.div_euclid(mat.columns), idx.rem_euclid(mat.columns)))
+        .filter(|&(row, col)| {
+            let neighbors = (-1_isize..2_isize)
+                .flat_map(|dr| {
+                    (-1_isize..2_isize).map(move |dc| {
+                        if dr == 0 && dc == 0 {
+                            return '.';
+                        }
+                        let neighbor_row = match isize::try_from(row) {
+                            Ok(idx) => idx.saturating_add(dr),
+                            Err(_) => return '.',
+                        };
+                        let neighbor_col = match isize::try_from(col) {
+                            Ok(idx) => idx.saturating_add(dc),
+                            Err(_) => return '.',
+                        };
+                        mat.at_signed(neighbor_row, neighbor_col).unwrap_or('.')
+                    })
+                })
+                .filter(|chr| *chr == '@')
+                .count();
+            neighbors <= 4
+        })
+        .count()
+}
+
+#[expect(clippy::print_stdout, reason = "This is a CLI function")]
 fn main() {
     env_logger::init();
     let input_path: &Path = Path::new("input.txt");
@@ -52,6 +96,11 @@ fn main() {
             exit(1)
         }
     };
-    let input = parse_input(&contents);
+    let input = parse_input(&contents).unwrap_or_else(|| {
+        error!("Could not parse input {contents}");
+        exit(1);
+    });
     info!("Parsed input: {input:?}");
+    let result = count_movable(&input);
+    println!("Result: {result}");
 }
