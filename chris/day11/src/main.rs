1c2da9a9ff53f2abd075a9a5ed2a04102f2361f7
Christopher Schölzel <christopher.schoelzel@languagetool.org>
2025-12-11 11:47:50 +0100

    gets mutable references and reduces lifetime of borrow

--- chris/day11/src/main.rs
+++ chris/day11/src/main.rs
@@ -22,14 +22,14 @@ struct Graph<'links> {
 }
 
 #[expect(
-    clippy::indexing_slicing,
+    clippy::unwrap_used,
     reason = "Cannot panic since we checked for existence. Need to use the field directly to avoid extra borrow."
 )]
 impl<'graph> Graph<'graph> {
     /// Get an existing node or add a new empty one and return that if it doesn't exist yet.
-    fn get_or_add(&'graph mut self, name: &str) -> &'graph Node<'graph> {
+    fn get_mut_or_add(&mut self, name: &str) -> &mut Node<'graph> {
         if self.nodes.contains_key(name) {
-            &self.nodes[name]
+            self.nodes.get_mut(name).unwrap()
         } else {
             self.add_empty(name)
         }
@@ -37,20 +37,27 @@ impl<'graph> Graph<'graph> {
 
     /// Adds a new empty node to the graph
     #[expect(
-        clippy::indexing_slicing,
+        clippy::unwrap_used,
         reason = "Panic cannot occur since we inserted the node right before referencing it."
     )]
-    fn add_empty(&mut self, name: &str) -> &Node<'graph> {
+    fn add_empty(&mut self, name: &str) -> &mut Node<'graph> {
         let new_node = Node {
             name: name.to_owned(),
             incoming: Vec::new(),
             outgoing: Vec::new(),
         };
         self.nodes.insert(name.to_owned(), new_node);
-        &self.nodes[name]
+        self.nodes.get_mut(name).unwrap()
     }
-    // fn add_node(mut self, name: &str, outgoing: Vec<&str>) {
-    //     let outgoing_nodes = outgoing.iter().map(|out_node| self.get_or_add(*out_node));
+
+    // /// Adds a node with outgoing edges, updating both forward and backward refefences
+    // fn add_node(&mut self, name: &str, outgoing: Vec<&str>) {
+    //     let new_node = self.get_mut_or_add(name);
+    //     for node_str in outgoing {
+    //         let node: &mut Node<'graph> = self.get_mut_or_add(node_str);
+    //         node.outgoing.push(new_node);
+    //     }
+    //     ()
     //     // match self.nodes.get(name) {
     //     //     Some(existing) =>
     //     // }
