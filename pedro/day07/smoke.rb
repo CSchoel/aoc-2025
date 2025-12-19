1b069a1495508212c896055d4162ef0ae74e3493
Pedro Goulart <pedro.goulart@languagetool.com>
2025-12-19 13:21:55 +0100

    Add ruby day07 part 1

---
+++ pedro/day07/smoke.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+num_splits = 0
+beam_locations = Set[]
+File.readlines("input.txt", chomp: true).each_with_index do |line, i|
+  fields = line.chars
+  if i.zero?
+    beam_locations << fields.index("S")
+    next
+  end
+  new_locations = Set[]
+  beam_locations.each do |loc|
+    if fields[loc] == "^"
+      beam_locations.delete(loc)
+      new_locations << loc.pred unless loc.zero?
+      new_locations << loc.next unless loc.pred == fields.size
+      num_splits += 1 unless new_locations.empty?
+    end
+  end
+  beam_locations.merge(new_locations)
+end
+
+p num_splits
