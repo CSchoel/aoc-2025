405b3401109e7202d166ff5bcaa59212d17d729e
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2025-12-27 22:42:27 +0100

    adds parser for sample input

--- chris/day07/src/main.rs
+++ chris/day07/src/main.rs
@@ -1,8 +1,48 @@
 //! Solves day 7 of Advent of code 2025
 
-use std::{env::args, fs, path::Path, process::exit};
+use std::{collections::HashSet, env::args, fs, path::Path, process::exit};
 
 use log::info;
+
+/// Represents a tachyon manifold
+#[derive(Debug)]
+struct TachyonManifold {
+    /// Starting column
+    start_pos: usize,
+    /// Splitter positions per row, starting from the top
+    splitter_positions: Vec<HashSet<usize>>,
+}
+
+/// Parses input string containing of a single line marking a starting position ('S')
+/// and multiple lines that can have splitters ('^'). Empty space is marked with a '.'.
+/// This ignores lines that only have empty space.
+fn parse_input(content: &str) -> TachyonManifold {
+    let mut start_pos: usize = 0;
+    let mut splitter_positions: Vec<HashSet<usize>> = Vec::new();
+    for line in content.lines() {
+        // skip empty lines
+        if line.chars().all(|chr| chr == '.') {
+            continue;
+        }
+        // set start pos if it exists
+        if let Some(s_idx) = line.find('S') {
+            start_pos = s_idx;
+            continue;
+        }
+        // not empty, no start => line with splitters
+        let positions = line
+            .chars()
+            .enumerate()
+            .filter_map(|(idx, chr)| (chr == '^').then_some(idx))
+            .collect::<HashSet<usize>>();
+        splitter_positions.push(positions);
+    }
+    let manifold = TachyonManifold {
+        start_pos,
+        splitter_positions,
+    };
+    manifold
+}
 
 #[expect(
     clippy::print_stdout,
@@ -23,10 +63,7 @@ fn main() {
             exit(1);
         }
     };
-    let Ok(input) = parse_input(&contents) else {
-        eprintln!("Could not parse input {contents}");
-        exit(1);
-    };
+    let input = parse_input(&contents);
     info!("Parsed input: {input:?}");
     println!("Result: TBD");
 }
