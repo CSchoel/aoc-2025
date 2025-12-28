ef0eb9b28e4b9ccb8548f52ea926b2bd3e2defa9
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2025-12-28 20:21:55 +0100

    parses input

--- chris/day08/src/main.rs
+++ chris/day08/src/main.rs
@@ -1,8 +1,38 @@
 //! Solves day 8 of Advent of Code 2025
 
-use std::{env::args, fs, path::Path, process::exit};
+use std::{env::args, fs, num::ParseIntError, path::Path, process::exit};
 
 use log::info;
+
+/// Represents a position in 3D carthesian coordinates
+#[derive(Debug)]
+struct Position3D {
+    /// Position on x axis
+    x: usize,
+    /// Position on y axis
+    y: usize,
+    /// Position on z axis
+    z: usize,
+}
+
+fn parse_input(content: &str) -> Result<Vec<Position3D>, String> {
+    content
+        .lines()
+        .map(|line| {
+            let Ok(numbers) = line
+                .split(',')
+                .map(str::parse::<usize>)
+                .collect::<Result<Vec<usize>, ParseIntError>>()
+            else {
+                return Err(format!("Could not parse numbers in line {line}"));
+            };
+            let &[x, y, z] = numbers.as_slice() else {
+                return Err(format!("Wrong number of coordinates: {numbers:?}"));
+            };
+            Ok(Position3D { x, y, z })
+        })
+        .collect()
+}
 
 #[expect(
     clippy::print_stdout,
