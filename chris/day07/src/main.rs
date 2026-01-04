f3d35a968eba6cde5b3c18b55f8148857eaf8f57
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2026-01-04 21:33:54 +0100

    adds solution for part 2

--- chris/day07/src/main.rs
+++ chris/day07/src/main.rs
@@ -2,7 +2,7 @@
 
 use std::{collections::HashSet, env::args, fs, path::Path, process::exit};
 
-use log::info;
+use log::{debug, info};
 
 /// Represents a tachyon manifold
 #[derive(Debug)]
@@ -66,6 +66,28 @@ fn count_splits(input: &TachyonManifold) -> usize {
     split
 }
 
+/// Counts the number of possible realities
+fn count_realities(input: &TachyonManifold, beam_x: usize, beam_y: usize) -> usize {
+    if beam_y == input.splitter_positions.len() {
+        // We've reached the end of the tachyon manifold, there is only one timeline here
+        return 1;
+    }
+    let empty_set = &HashSet::new();
+    let splitters = input.splitter_positions.get(beam_y).unwrap_or(empty_set);
+    debug!("Checking for split: splitters = {splitters:?}, beam_x = {beam_x}.");
+    if splitters.contains(&beam_x) {
+        info!("Splitting timeline at ({beam_x}, {beam_y})");
+        // Beam will be split => We split into two different timelines
+        let left_realities =
+            count_realities(input, beam_x.saturating_sub(1), beam_y.saturating_add(1));
+        let right_realities =
+            count_realities(input, beam_x.saturating_add(1), beam_y.saturating_add(1));
+        return left_realities.saturating_add(right_realities);
+    }
+    // Beam is not split, just continue
+    count_realities(input, beam_x, beam_y.saturating_add(1))
+}
+
 #[expect(
     clippy::print_stdout,
     clippy::print_stderr,
@@ -87,6 +109,8 @@ fn main() {
     };
     let input = parse_input(&contents);
     info!("Parsed input: {input:?}");
-    let result = count_splits(&input);
-    println!("Result: {result}");
+    let result_part1 = count_splits(&input);
+    println!("Number of splits: {result_part1}");
+    let result_part2 = count_realities(&input, input.start_pos, 0);
+    println!("Number of realities: {result_part2}");
 }
